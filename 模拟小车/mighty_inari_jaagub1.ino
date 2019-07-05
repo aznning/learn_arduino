6725625daea24a07711b5309a12eedc5455a7bf3
aznning <52487270+aznning@users.noreply.github.com>
2019-07-05 12:10:29 +0800

    Add files via upload

---
+++ 模拟小车/mighty_inari_jaagub1.ino
@@ -0,0 +1,68 @@
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  Serial.begin(9600);
+}
+int income=0;
+void loop()
+{
+  if(Serial.available()>0)
+{income=Serial.read();
+  switch(income)
+{case'f':
+ forward();
+ break;
+ case'b':
+ backward();
+ break;
+ case'l':
+ leftward();
+ break;
+ case'r':
+ rightward();
+ break;
+}
+}
+}
+ void forward()
+ {
+   digitalWrite(9,HIGH);
+   digitalWrite(10,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,LOW);
+   delay(1000);
+ }
+ void backward()
+ { digitalWrite(10,HIGH);
+   digitalWrite(9,LOW);
+   digitalWrite(6,HIGH);
+   digitalWrite(5,LOW);
+   digitalWrite(3,HIGH);
+   digitalWrite(4,LOW);
+   delay(1000);
+ }
+  void leftward()
+  {digitalWrite(9,LOW);
+   digitalWrite(10,LOW);
+   digitalWrite(5,HIGH);
+   digitalWrite(6,LOW);
+   digitalWrite(11,HIGH);
+   digitalWrite(12,LOW);
+   delay(1000);
+  }
+ void rightward()
+ { digitalWrite(9,HIGH);
+   digitalWrite(10,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(6,LOW);
+   digitalWrite(11,HIGH);
+   digitalWrite(12,LOW);
+   delay(1000);
+ }
